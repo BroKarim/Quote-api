1999385bc54bb6e55da018dfaf89af03215dd9eb
BroKarim <146515339+BroKarim@users.noreply.github.com>
2023-10-15 20:52:15 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-/*
-- KITA SALAH NAMA, yang betul itu gallery websote
-- Ndak masalah toh g nampah
-
-URUTAN
-- Create folder pake tailwind
-- Rapiin file
-- Tes run pake hello world
-- Mulai buat Navbar
-
-*/
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Hero from './pages/Hero';
 import QuotePage from './pages/QuotePage';
 
 function App() {
   return (
     <>
-      {/* <Hero></Hero> */}
       <QuotePage></QuotePage>
 
     </>
